525c43772fe2fe0f222681c8adeda8e0d7fa5a8d
Xanders Wachira <muhindia66@gmail.com>
2024-01-16 22:56:16 -0800

    The use of reading a txt file in order to reduce the word count within the script.

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = [str.split()[7] str.split()[8] str.split()[14] str.split()[0]]
+print(str)
