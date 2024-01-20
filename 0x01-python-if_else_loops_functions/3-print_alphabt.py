d541203b24303a5fb4ffdf7d8027041b3cdd06c4
Xanders Wachira <muhindia66@gmail.com>
2024-01-20 00:05:25 -0800

    The conversion of Decimal to Hexadecimal

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z')+1):
     if letter not in (ord('e'), ord('q')):
-        print(f'{chr(letter)}', end='')
+        print('{}'.format(chr(letter)), end='')
